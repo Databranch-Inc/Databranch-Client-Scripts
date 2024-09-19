3a1e1df7ddd7d2e1e0ec8d9d557d6746ac940fe2
JoshBritton5544 <54032944+JoshBritton5544@users.noreply.github.com>
2024-09-19 16:37:43 -0400

    1

---
+++ Windows Services and Processes/CheckandSetServiceStartup.ps1
@@ -0,0 +1,25 @@
+<#
+Check for and set service Startup
+
+This script will take a service name as a variable from CW Automate, check that it exists, and set the startup type to Automatic
+
+1.0
+
+Josh Britton
+
+Original Date - 
+Last Commit Date/Version - 
+
+==========================================================================================================================
+1.0
+
+Original script write.
+==========================================================================================================================
+#>
+
+Function Set-ServiceStartup{
+
+#VariableSet
+$Service = 
+
+}
