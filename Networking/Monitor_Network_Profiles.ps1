cd252a30c8267afe0e9f76beed05a93c006f21f9
JoshBritton5544 <54032944+JoshBritton5544@users.noreply.github.com>
2024-10-25 14:07:41 -0400

    JB 10-25

---
+++ Networking/Monitor_Network_Profiles.ps1
@@ -0,0 +1,19 @@
+<#Monitor Network Profiles
+
+This script is desiged to gather information regarding networking profiles on a machine. Specifically, this is looking for the network type to ensure that servers keep Domain Network Profiles.
+
+Data from this can be exported/gathered into CW Automate, where we can alert as needed
+
+Josh Britton
+
+1.0
+
+Initial Write 10-3-24
+Last Update
+
+=====================================================================================
+1.0
+
+Script Initial Write
+=====================================================================================
+#>
