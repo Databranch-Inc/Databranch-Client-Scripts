e896bf9eb1748ec17e20f4dffc1f3ff14ab1f7af
JoshBritton5544 <54032944+JoshBritton5544@users.noreply.github.com>
2024-12-05 14:03:28 -0500

    JB - 12-5

--- PC Maintenance/Databranch_Folder_Cleanup.ps1
+++ PC Maintenance/Databranch_Folder_Cleanup.ps1
@@ -1,1 +1,56 @@
-<##>
+<#Databranch Folder Cleanup
+
+This script reviews and cleans items over a month old in the C:\Databranch folder, along with its subfolders.
+
+Origional Creation Date - 11-11-24
+Josh Britton
+
+Version 1.0
+=========================================================================================================
+Version 1.0
+
+Origional Write
+#>
+
+#Variable SSet
+
+#Set Date and target date to delete items
+
+Function Cleanup-FolderDatabranch{
+
+#Current Date set to format to match Get-Childitem output
+$CurrentDate = get-date -Format "MM/dd/yyyy h:mm tt"
+
+#Set Delete Date for 30 Days ago
+$30DaysAgo =(Get-Date).AddDays(-30)
+$RemoveDate = Get-Date -Date $30DaysAgo -Format "MM/dd/yyyy h:mm:ss tt"
+
+
+#Set Transcription Date for Logging
+$TranscriptDate = Get-Date -Format "MM-dd-yyyy_hhmmsstt"
+
+#Parent Folder
+$ParentFolder = "C:\Databranch"
+
+#Start Transcript for Cleanup Actions:
+Start-Transcript -Path "C:\Databranch\Logs\Databranch_Folder)Cleanup_Script_Logs_$TranscriptDate.txt" -NoClobber
+$Transcript = "C:\Databranch\Logs\Databranch_Folder)Cleanup_Script_Logs_$TranscriptDate.txt"
+
+$OldFiles = Get-ChildItem -Path $ParentFolder -Recurse | Where-Object -Property LastWriteTime -LT $RemoveDate
+
+foreach ($OldFile in $OldFiles){
+    Remove-Item -Force
+    Write-Host - 
+}
+
+$EmptyFolders = Get-ChildItem -Path $ParentFolder -Directory | Where-Object { $_.GetFileSystemInfos().Count -eq 0 -and $_.Name -ne "Logs"}
+
+Foreach ($EmptyFolder in $EmptyFolders){
+
+    Remove-Item -Force
+}
+
+#Stop Transcript of File Delete Actions
+Stop-Transcript
+
+}
