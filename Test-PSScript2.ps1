39f670b08e18058be994aa88d18374728406b75c
generic-MSP-engineer <samk.databranch.lists@gmail.com>
2026-02-22 12:20:04 -0500

    Update Test-PSScript2.ps1

--- Test-PSScript2.ps1
+++ Test-PSScript2.ps1
@@ -1,2 +1,3 @@
-﻿##ENGINEERSPOWERAPP#TAGS#maintenance,deployment
+﻿##SCRIPTMGMT#COMMENT#this is a test app||this||is||extra||lines
+##SCRIPTMGMT#TAGS#maintenance,deployment
 Write-Host "This is a test app"
